86fa3a274ab89f2e24eeaf3333f169b9668ff635
Moses Ugwu <ugwumoses1992@gmail.com>
2024-06-24 14:33:26 +0100

    used mysql role to create mysql_db and encrypted my password

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -7,22 +7,22 @@ locals {
       username       = "mosesugwu"
       size           = "Standard_F2"
     },
-    vm-2 = {
-      name           = data.azurecaf_name.vm_2.result
-      size           = "Standard_F2"
-      public_key     = file("moseskey.pub")
-      admin_username = "mosesugwu"
-      username       = "mosesugwu"
+    # vm-2 = {
+    #   name           = data.azurecaf_name.vm_2.result
+    #   size           = "Standard_F2"
+    #   public_key     = file("moseskey.pub")
+    #   admin_username = "mosesugwu"
+    #   username       = "mosesugwu"
 
-    },
-    vm-3 = {
-      name           = data.azurecaf_name.vm_3.result
-      size           = "Standard_F2"
-      public_key     = file("moseskey.pub")
-      admin_username = "mosesugwu"
-      username       = "mosesugwu"
+    # },
+    # vm-3 = {
+    #   name           = data.azurecaf_name.vm_3.result
+    #   size           = "Standard_F2"
+    #   public_key     = file("moseskey.pub")
+    #   admin_username = "mosesugwu"
+    #   username       = "mosesugwu"
       
-    }
+    # }
   }
   network_interface_ids = {
     vm-1 = {
@@ -30,16 +30,16 @@ locals {
       public_ip_address_id = module.virtual_network.pip_id 
 
     },
-    vm-2 = {
-      name                 = data.azurecaf_name.nic_2.result
-      public_ip_address_id = azurerm_public_ip.vm_2.id
+    # vm-2 = {
+    #   name                 = data.azurecaf_name.nic_2.result
+    #   public_ip_address_id = azurerm_public_ip.vm_2.id
 
-    },
-    vm-3 = {
-      name                 = data.azurecaf_name.nic_3.result
-      public_ip_address_id = azurerm_public_ip.vm_3.id
+    # },
+    # vm-3 = {
+    #   name                 = data.azurecaf_name.nic_3.result
+    #   public_ip_address_id = azurerm_public_ip.vm_3.id
 
-    }
+    # }
 
   }
 }
